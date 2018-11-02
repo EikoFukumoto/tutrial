4fde1378666f87c128f0f64809f8e8a7b74c8150
EikoFukumoto <eiko.fukumoto@shimz.co.jp>
2018-11-02 17:47:02 +0900

    機能追加

--- MovieCap/MovieCap/WMain.Designer.cs
+++ MovieCap/MovieCap/WMain.Designer.cs
@@ -41,6 +41,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnEnd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -160,11 +161,21 @@
             this.btnEnd.UseVisualStyleBackColor = true;
             this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(773, 624);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // WMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(961, 715);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnEnd);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.panel1);
@@ -172,7 +183,7 @@
             this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "WMain";
-            this.Text = "MovieCap（映像キャプチャ）";
+            this.Text = "MovieCap（機能追加）";
             this.Load += new System.EventHandler(this.WMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.panel1.ResumeLayout(false);
@@ -195,5 +206,6 @@
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnConfig;
         private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button button1;
     }
 }
